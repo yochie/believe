ac986833cf0572a4e929a4363a355a24609ba773
Yoann Ochietti <yoann.ochietti@gmail.com>
2024-03-19 17:49:33 -0400

    Gravity and jumping

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,36 +5,68 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private CharacterController controller;
+    
+    [SerializeField] private Camera cam;
 
     [SerializeField] private float moveSpeed;
 
     [Range(0,1)]
     [SerializeField] private float rotationSpeed;
 
-    [SerializeField] private Camera cam;
+    [SerializeField] private float jumpSpeed;
 
-    private Vector3 horizontalMovementInput;
+    [SerializeField] private float gravityScale;    
+
+    private Vector3 horizontalMovemInput;
+
+    private readonly float baseGravity = 9.81f;
 
     // Start is called before the first frame update
     void Start()
     {
-        this.horizontalMovementInput = Vector3.zero;
+        this.horizontalMovemInput = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.horizontalMovementInput = new Vector3 (Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
-        if (horizontalMovementInput.magnitude > 0)
-        {
-            float camRotation = cam.transform.rotation.eulerAngles.y;
-            Vector3 moveDir = (Quaternion.Euler(0, camRotation, 0) * this.horizontalMovementInput);
-            this.controller.Move(moveDir * this.moveSpeed * Time.deltaTime);
+        Vector3 velocity = Vector3.zero;
 
-            //rotation
-            Quaternion rotateTowardsMoveDirection = Quaternion.LookRotation(moveDir);
+        this.horizontalMovemInput = new Vector3 (Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        if (horizontalMovemInput.magnitude > 0)
+        {
+            //horizontal movement
+            float horizontalCamRotation = cam.transform.rotation.eulerAngles.y;
+            Vector3 horizontalMoveDirection = Quaternion.Euler(0, horizontalCamRotation, 0) * this.horizontalMovemInput;
+            Vector3 horizontalVelocity = horizontalMoveDirection * this.moveSpeed;
+            velocity.x = horizontalVelocity.x;
+            velocity.z = horizontalVelocity.z;
+
+
+            //rotate in movement direction
+            Quaternion rotateTowardsMoveDirection = Quaternion.LookRotation(horizontalMoveDirection);
             Quaternion smoothedRotation = Quaternion.Slerp(transform.rotation, rotateTowardsMoveDirection, this.rotationSpeed);
             this.transform.rotation = smoothedRotation;
         }
+
+        //vertical movement
+        Debug.Log(controller.isGrounded);
+        if (controller.isGrounded)
+        {
+            if (this.IsJumping())
+                velocity.y = this.jumpSpeed;
+            else
+                velocity.y = -this.baseGravity * this.gravityScale;
+        }
+        else
+        {
+            velocity.y = this.controller.velocity.y - (this.baseGravity * this.gravityScale * Time.deltaTime);
+        }
+        this.controller.Move(velocity * Time.deltaTime);
+    }
+
+    bool IsJumping()
+    {
+        return Input.GetKeyDown(KeyCode.Space);
     }
 }
