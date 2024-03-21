bbfb1130fb72897c13f807a782f6fdbaccb423e3
Yoann Ochietti <yoann.ochietti@gmail.com>
2024-03-21 17:36:02 -0400

    Unity player controller started asset imported. Scripts copied and modified to add flight support.

--- Assets/Scripts/old/PlayerController.cs
+++ Assets/Scripts/old/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,30 +16,41 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] private float jumpSpeed;
 
+    [SerializeField] private float jumpCooldown;
+
+
     [SerializeField] private float gravityScale;    
 
     private Vector3 horizontalMovemInput;
 
     private readonly float baseGravity = 9.81f;
 
+    private float jumpCooldownRemaining;
+
+    private bool jumped;
+
     // Start is called before the first frame update
     void Start()
     {
         this.horizontalMovemInput = Vector3.zero;
+        this.jumped = false;
+        this.jumpCooldownRemaining = 0;
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Vector3 velocity = Vector3.zero;
 
-        this.horizontalMovemInput = new Vector3 (Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
         if (horizontalMovemInput.magnitude > 0)
         {
             //horizontal movement
             float horizontalCamRotation = cam.transform.rotation.eulerAngles.y;
             Vector3 horizontalMoveDirection = Quaternion.Euler(0, horizontalCamRotation, 0) * this.horizontalMovemInput;
-            Vector3 horizontalVelocity = horizontalMoveDirection * this.moveSpeed;
+            Vector3 floorNormal = this.GetFloorNormal();
+            Vector3 moveDirectionAlongSlope = Vector3.ProjectOnPlane(horizontalMoveDirection, floorNormal).normalized;
+            Vector3 horizontalVelocity = moveDirectionAlongSlope * this.moveSpeed;
             velocity.x = horizontalVelocity.x;
             velocity.z = horizontalVelocity.z;
 
@@ -50,23 +62,50 @@ public class PlayerController : MonoBehaviour
         }
 
         //vertical movement
-        Debug.Log(controller.isGrounded);
         if (controller.isGrounded)
         {
-            if (this.IsJumping())
+            if (this.jumped)
+            {
+                //controller.gro
                 velocity.y = this.jumpSpeed;
+            }
             else
+            {
+                //constant down force needed for character controller to properly detect if grounded
                 velocity.y = -this.baseGravity * this.gravityScale;
+            }
         }
         else
         {
-            velocity.y = this.controller.velocity.y - (this.baseGravity * this.gravityScale * Time.deltaTime);
+            velocity.y = this.controller.velocity.y - (this.baseGravity * this.gravityScale * Time.fixedDeltaTime);
         }
-        this.controller.Move(velocity * Time.deltaTime);
+        this.controller.Move(velocity * Time.fixedDeltaTime);
+        this.jumped = false;
     }
 
-    bool IsJumping()
+    private void Update()
     {
-        return Input.GetKeyDown(KeyCode.Space);
+        this.jumpCooldownRemaining -= Time.deltaTime;
+        if(Input.GetKeyDown(KeyCode.Space) && this.jumpCooldownRemaining <= 0)
+        {
+            this.jumped = true;
+            this.jumpCooldownRemaining = this.jumpCooldown;
+        }
+        
+        this.horizontalMovemInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
     }
+    
+    private Vector3 GetFloorNormal()
+    {
+        RaycastHit hit;
+        float distToGround = controller.bounds.extents.y - controller.center.y;
+        if (controller.isGrounded && Physics.Raycast(transform.position, -transform.up, out hit, distToGround + 1f))
+        {
+            Vector3 normal = hit.normal;
+            return normal;
+        } else
+            return Vector3.up;
+    }
+
+    
 }
