2668d4523e36edb4b3fd38adf820ff227c7eb3c9
Yoann Ochietti <yoann.ochietti@gmail.com>
2024-03-27 13:35:59 -0400

    Wind state and indicator updated to include randomness and better icon.

--- Assets/WindIndicator.cs
+++ Assets/WindIndicator.cs
@@ -10,13 +10,15 @@ public class WindIndicator : MonoBehaviour
     [Tooltip("Counter clockwise angle from default arrow position to north pointing")]
     [SerializeField] private float windArrowOffset;
     [SerializeField] private WindState windState;
+    [Range(0,1)]
+    [SerializeField] private float smoothFactor;
 
     private float camYaw;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        //initialize without smoothing
+        this.windArrow.localRotation = Quaternion.Euler(0, 0, this.windState.GetWindDirection() + this.windArrowOffset);
     }
 
     // Update is called once per frame
@@ -24,11 +26,13 @@ public class WindIndicator : MonoBehaviour
     {
         this.camYaw = cam.transform.rotation.eulerAngles.y;
         this.transform.rotation = Quaternion.Euler(0, 0, camYaw);
-        this.SetWindDirection(this.windState.GetWindDirection());
+        this.UpdateArrow(this.windState.GetWindDirection());
     }
 
-    public void SetWindDirection(float counterClockwiseAngleFromNorth)
+    public void UpdateArrow(float counterClockwiseAngleFromNorth)
     {
-        this.windArrow.localRotation = Quaternion.Euler(0, 0, counterClockwiseAngleFromNorth + this.windArrowOffset);
+        Quaternion target = Quaternion.Euler(0, 0, counterClockwiseAngleFromNorth + this.windArrowOffset);        
+        this.windArrow.localRotation = Quaternion.Lerp(this.windArrow.localRotation, target, this.smoothFactor * Time.deltaTime);
     }
+
 }
--- Assets/WindState.cs
+++ Assets/WindState.cs
@@ -8,8 +8,42 @@ public class WindState : MonoBehaviour
     [Tooltip("Counter clock-wise angle from north")]
     [SerializeField] private float windDirection;
 
+    [SerializeField] private float changeInterval;
+
+    [Tooltip("Degree delta per interval")]
+    [Range(0, 360)]
+    [SerializeField] private float changeRate;
+
+    private void Awake()
+    {
+        //init to random position along increments
+        this.windDirection = UnityEngine.Random.Range(0, (int) (360 / changeRate)) * changeRate;
+    }
+
+    private void Start()
+    {
+
+        this.StartCoroutine(RandomizeWindDirection());
+    }
+
+    private void Update()
+    {
+
+    }
+
     internal float GetWindDirection()
     {
         return this.windDirection;
+    }
+
+    private IEnumerator RandomizeWindDirection()
+    {
+        while (true)
+        {
+            int changeDir = UnityEngine.Random.Range(-1, 2);
+            this.windDirection += changeDir * changeRate;
+            this.windDirection %= 360;
+            yield return new WaitForSeconds(this.changeInterval);
+        }
     }
 }
